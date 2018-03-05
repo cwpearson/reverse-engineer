219980ee88d08c08ddf000118e53bc57c8ff91f9
Carl Pearson <pearson@illinois.edu>
2018-03-05 16:11:43 -0600

    script to re-run timelines

---
+++ make-timelines.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -eu
+
+MACHINE=$1
+
+make
+
+for b in pageable_vs_pinned; do
+    mkdir -pv "$b"/results/"$MACHINE"
+    nvprof -o "$b"/results/"$MACHINE"/timeline.nvvp -f "$b"/main
+done
